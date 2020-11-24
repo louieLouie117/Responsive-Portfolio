e4c56d4a4b0a69ae4d37aa92803d2f8fb271c350
Luis Cardona <cardona-luis@outlook.com>
2020-11-23 20:02:54 -0800

    show ditails

--- client/src/views/Projects.jsx
+++ client/src/views/Projects.jsx
@@ -1,7 +1,60 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 
 const Projects = props => {
+    const [project1Details, setProject1Details] = useState(false)
+    const [project1Img, setProject1Img] = useState(false)
+
+    const [project2Details, setProject2Details] = useState(false)
+    const [project2Img, setProject2Img] = useState(false)
+
+    const [project3Details, setProject3Details] = useState(false)
+    const [project3Img, setProject3Img] = useState(false)
+
+
+
+
+    function project1State() {
+        setProject1Details(!project1Details);
+        setProject1Img(!project1Img);
+
+        setProject2Details(false);
+        setProject2Img(false);
+        setProject3Details(false);
+        setProject3Img(false);
+    
+    }
+
+
+    function project2State() {
+        setProject2Details(!project2Details);
+        setProject2Img(!project2Img);
+
+        setProject1Details(false);
+        setProject1Img(false);
+        setProject3Details(false);
+        setProject3Img(false);
+
+        
+    }
+
+    
+    function project3State() {
+        setProject3Details(!project3Details);
+        setProject3Img(!project3Img);
+        
+        setProject1Details(false);
+        setProject1Img(false);
+        setProject2Details(false);
+        setProject2Img(false);
+    
+    }
+
+
+ 
+
+
+
     return (
         <div className="projectsPage-container">
 
@@ -28,9 +81,17 @@ const Projects = props => {
                     <ul>
                         <li className="project1-container">
                             <h2>CRUD Web App</h2>
-                            <img src="/img/projectImg1.png" alt=""/>
+                            <img 
+                            src="/img/projectImg1.png" 
+                            style={{ filter: project1Img ? "blur(4px)" : "blur(0px)" }} />
+
+                            <div  
+                            style={{ display: project1Details ? "grid" : "none" }} 
+                            className="tools-container">
+                                <p>need this to be grid</p>
+                                </div>
                             <footer> 
-                                <a href="#">Details</a>
+                                <a  onClick={project1State}>Details</a>
                                 <button id="projectOneBTN">Try it.</button>
                             </footer>
                         </li>
@@ -38,18 +99,32 @@ const Projects = props => {
 
                         <li className="project2-container">
                             <h2>CRUD Web App</h2>
-                            <img src="/img/projectImg2.png" alt=""/>
+                            <img src="/img/projectImg2.png" 
+                            style={{ filter: project2Img ? "blur(4px)" : "blur(0px)" }}/>
+                            <div  
+                            style={{ display: project2Details ? "grid" : "none" }} 
+                            className="tools-container">
+                                <p>need this to be grid</p>
+                                </div>
                             <footer> 
-                                <a href="#">Details</a>
+                                <a onClick={project2State}>Details</a>
                                 <button>Try it.</button>
                             </footer>
                         </li>
 
                         <li className="project3-container">
                             <h2>CRUD Web App</h2>
-                            <img src="/img/projectImg3.png" alt=""/>
+                            <img 
+                            src="/img/projectImg3.png" 
+                            style={{ filter: project3Img ? "blur(4px)" : "blur(0px)" }}/>
+                            <div  
+                            style={{ display: project3Details ? "grid" : "none" }} 
+                            className="tools-container">
+                                <p>need this to be grid</p>
+                                </div>
+
                             <footer> 
-                                <a href="#">Details</a>
+                                <a onClick={project3State}>Details</a>
                                 <button>Visit it.</button>
                             </footer>
                         </li>
